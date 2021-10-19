3169e331a44b83be4fb678c17f39509e2be399a5
Sw <86825656+swlee9087@users.noreply.github.com>
2021-10-19 18:09:58 +0900

    18

--- backend/admin/tensor/models.py
+++ backend/admin/tensor/models.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
 from admin.common.models import ValueObject
 
 
@@ -11,96 +10,16 @@ class FashionClassification(object):
     def __init__(self):
         self.vo = ValueObject()
         self.vo.context = 'admin/tensor/data/'
-        self.class_name = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+        self.class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
     def fashion(self):  # django
-        # self.get_data()
-        self.hook()
-
-    def hook(self):  # 전체 다 걸림
-        # images = self.get_data()
-        [train_images, train_labels, test_images, test_labels] = self.get_data()
-        model = self.create_model()
-        # model = self.train_model(model, X_train_full, y_train_full)
-        # model = self.train_model(model, ls[0], ls[1])  NOPE
-        # self.test_model(model)
-        # arr = self.predict(model, ls[2], ls[3], 0)  # leave index val as 0
-        # prediction = arr[0]
-        # test_images = arr[1]
-        # test_labels = arr[2]
-        model.fit(train_images, train_labels, epochs=5)
-        test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
-        # verbose 는 학습하는 내부상황 보기 중 2번선택
-        predictions = model.predict(test_images)
-        i = 5  # change for diff pic
-        # print(f'prediction: {prediction}')
-        # print(f'test img: {prediction}')
-        # print(f'test val: {prediction}')
-        # pred = predictions[i]
-        # answer = test_labels[i]
-        print(f'모델이 예측한 값 {np.argmax(predictions)}')
-        print(f'정답: {test_labels[i]}')
-        print(f'테스트 정확도: {test_acc}')
-        plt.figure(figsize=(6,3))
-        plt.subplot(1,2,1)  # row 1 col 2 position 1st
-        # prediction_array, true_label, image = predictions[i], test_labels[i], test_images[i]
-        test_image, test_predictions, test_label = test_images[i], predictions[i], test_labels[i]
-        plt.grid(False)
-        plt.xticks([])
-        plt.yticks([])
-        plt.imshow(test_image, cmap=plt.cm.binary)
-        test_pred = np.argmax(test_predictions)
-        print('f{test_pred}')
-        print('#' * 100)
-        print('f{test_label}')
-        # if test_pred == test_label:
-        #     color = 'blue'
-        # else:
-        #     color = 'red'
-        # plt.xlabel('{} : {}%'.format(self.class_names[test_pred],
-        #                              100 * np.max(test_predictions),
-        #                              self.class_names[test_label], color))
-        plt.subplot(1,2,2)
-        plt.grid(False)
-        plt.xticks([])
-        plt.yticks([])
-        this_plot = plt.bar(range(10), test_pred, color='#777777')
-        plt.ylim([0,1])
-        test_pred = np.argmax(test_predictions)
-        this_plot[test_pred].set_color('red')
-        this_plot[test_label].set_color('blue')
-        plt.savefig(f'{self.vo.context}fashion_answer.png')
-
-    def get_data(self) -> []:
         fashion_mnist = keras.datasets.fashion_mnist
-        (train_images, train_labels),(test_images, test_labels) = fashion_mnist.load_data()
+        (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
         # self.peek_datas(X_train_full, X_test, y_train_full)
         # return [X_train_full, y_train_full, X_test, y_test]  # ls base
-        return [train_images, train_labels, test_images, test_labels]
+        # return [train_images, train_labels, test_images, test_labels]
 
-    def peek_datas(self, train_images, test_images, train_labels):
-        print(train_images.shape)
-        print(train_images.dtype)
-        print(f'train 행 : {train_images.shape[0]} 열 : {train_images.shape[1]}')
-        print(f'test 행 : {test_images.shape[0]} 열 : {test_images.shape[1]}')
-        plt.figure()
-        plt.imshow(train_images[3])
-        plt.colorbar()
-        plt.grid(False)
-        plt.savefig(f'{self.vo.context}fashion_random.png')
-
-        plt.figure(figsize=(10, 10))
-        for i in range(25):
-            plt.subplot(5, 5, i + 1)
-            plt.xticks([])
-            plt.yticks([])
-            plt.grid(False)
-            plt.imshow(train_images[i], cmap=plt.cm.binary)
-            plt.xlabel(self.class_name[train_labels[i]])
-        plt.savefig(f'{self.vo.context}fashion_subplot.png')
-
-    def create_model(self) -> object:
         # model = keras.models.Sequential()  # "sequential model"
         # model.add(keras.layers.Flatten(input_shape=[28, 28]))  # input(matrix) layer flat.
         # # also sets pixel size for fshrdm.png
@@ -116,11 +35,85 @@ class FashionClassification(object):
         # model.summary()  # Tf book p375
         # model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
         model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-        return model
+        # return model
+        model.fit(train_images, train_labels, epochs=5)
+        self.test_and_save_images(model,test_images, test_labels)
+        # model.save(f'{self.vo.context}fashion_classification.h5')
 
-    # def train_model(self, model, train_images, train_labels) -> object:
-    #     model.fit(train_images, train_labels, epoch=5)
-    #     return model
+    def peek_datas(self, train_images, test_images, train_labels):
+        print(train_images.shape)
+        print(train_images.dtype)
+        print(f'train 행 : {train_images.shape[0]} 열 : {train_images.shape[1]}')
+        print(f'test 행 : {test_images.shape[0]} 열 : {test_images.shape[1]}')
+        plt.figure()
+        plt.imshow(train_images[3])
+        plt.colorbar()
+        plt.grid(False)
+        plt.savefig(f'{self.vo.context}fashion_random.png')
+        plt.figure(figsize=(10, 10))
+        for i in range(25):
+            plt.subplot(5, 5, i + 1)
+            plt.xticks([])
+            plt.yticks([])
+            plt.grid(False)
+            plt.imshow(train_images[i], cmap=plt.cm.binary)
+            plt.xlabel(self.class_name[train_labels[i]])
+        plt.savefig(f'{self.vo.context}fashion_subplot.png')
+
+    def test_and_save_images(self, model, test_images, test_labels):  # 전체 다 걸림
+        # images = self.get_data()
+        # [train_images, train_labels, test_images, test_labels] = self.get_data()
+        # model = self.create_model()
+        # model = self.train_model(model, X_train_full, y_train_full)
+        # model = self.train_model(model, ls[0], ls[1])  NOPE
+        # self.test_model(model)
+        # arr = self.predict(model, ls[2], ls[3], 0)  # leave index val as 0
+        # prediction = arr[0]
+        # test_images = arr[1]
+        # test_labels = arr[2]
+        # model.fit(train_images, train_labels, epochs=5)
+        test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+        # verbose 는 학습하는 내부상황 보기 중 2번선택
+        predictions = model.predict(test_images)
+        i = 5  # change for diff pic
+        # print(f'prediction: {prediction}')
+        # print(f'test img: {prediction}')
+        # print(f'test val: {prediction}')
+        # pred = predictions[i]
+        # answer = test_labels[i]
+        print(f'모델이 예측한 값 {np.argmax(predictions)}')
+        print(f'정답: {test_labels[i]}')
+        print(f'테스트 정확도: {test_acc}')
+        plt.figure(figsize=(6, 3))
+        plt.subplot(1, 2, 1)  # row 1 col 2 position 1st
+        # prediction_array, true_label, image = predictions[i], test_labels[i], test_images[i]
+        test_image, test_predictions, test_label = test_images[i], predictions[i], test_labels[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(test_image, cmap=plt.cm.binary)
+        test_pred = np.argmax(test_predictions)
+        # print('f{test_pred}')
+        # print('#' * 100)
+        # print('f{test_label}')
+        if test_pred == test_label:
+            color = 'blue'
+        else:
+            color = 'red'
+        plt.xlabel('{} : {}%'.format(self.class_names[test_pred],
+                                     100 * np.max(test_predictions),
+                                     self.class_names[test_label], color))
+        plt.subplot(1, 2, 2)
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+        this_plot = plt.bar(range(10), test_pred, color='#777777')
+        plt.ylim([0, 1])
+        test_pred = np.argmax(test_predictions)
+        this_plot[test_pred].set_color('red')
+        this_plot[test_label].set_color('blue')
+        plt.savefig(f'{self.vo.context}fashion_answer2.png')
+
     #
     # def test_model(self, model, test_images, test_labels) -> object:
     #     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
@@ -154,7 +147,7 @@ class AdalineGD(object):  # 적응형 선형 뉴런 분류기.  GD = gradient de
 
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
-        self.cost_ = []  #  accum 비용 함수의 제곱 sum per epoch
+        self.cost_ = []  # accum 비용 함수의 제곱 sum per epoch
 
         for i in range(self.n_iter):
             net_input = self.net_input(X)
